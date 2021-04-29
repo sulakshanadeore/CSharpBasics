5fa8508a2fcdbe976734c5a8dffff81f1eb662e4
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2021-04-29 16:06:57 +0530

    constructor

--- HRApp/Program.cs
+++ HRApp/Program.cs
@@ -16,17 +16,17 @@ namespace HRApp
 
             //Default Constructor of the class
             PropEmployee emp1 = new PropEmployee();
-          //  Console.WriteLine("Enter Employee Id");
-          //emp1.EmployeeId = Convert.ToInt32(Console.ReadLine());
+            //  Console.WriteLine("Enter Employee Id");
+            //emp1.EmployeeId = Convert.ToInt32(Console.ReadLine());
 
-          //  Console.WriteLine("Enter employee Name");
-          //  emp1.EmployeeName = Console.ReadLine();
+            //  Console.WriteLine("Enter employee Name");
+            //  emp1.EmployeeName = Console.ReadLine();
 
-          //  Console.WriteLine("Enter Deptno");
-          //  emp1.DepartmentNo = Convert.ToInt32(Console.ReadLine());
+            //  Console.WriteLine("Enter Deptno");
+            //  emp1.DepartmentNo = Convert.ToInt32(Console.ReadLine());
 
-          //  Console.WriteLine("Enter Salary");
-          //  emp1.Salary = Convert.ToSingle(Console.ReadLine());
+            //  Console.WriteLine("Enter Salary");
+            //  emp1.Salary = Convert.ToSingle(Console.ReadLine());
 
             //emp1.Password = "temp@345";
             //emp1.NetSalary = 3333;
@@ -38,11 +38,31 @@ namespace HRApp
             //Console.WriteLine(emp1.DepartmentNo);
             //Console.WriteLine(emp1.Salary);
             //     Console.WriteLine(emp1.NetSalary);
-            Console.WriteLine(emp1.Password);
-            Console.WriteLine("Please enter new password");
-            emp1.NewPassword = Console.ReadLine();
-            
-            
+            //Console.WriteLine(emp1.Password);
+            //Console.WriteLine("Please enter new password");
+            //emp1.NewPassword = Console.ReadLine();
+
+
+            //Orders o = new Orders(12,"Mobile Phone",40000,1);
+            //o.OrderNo = 13;
+            //o.OrderDate = new DateTime(2021,04,29);
+            //Console.WriteLine(o.OrderAmt);
+           
+            //Orders o1 = new Orders(12, "Mobile Phone", 50000, 1);
+            //o.OrderNo = 14;
+            //o.OrderDate = new DateTime(2021, 04, 29);
+            //Console.WriteLine(o1.OrderAmt);
+
+            //Orders o2 = new Orders();
+            //Console.WriteLine(o2.OrderAmt);
+
+            Orders o4 = new Orders(123, "Hard disk",10000f);
+                /* Polymorphism---- Overloading Constructors*/
+            //Same  name but different number,type and sequence of arguments to the method--- function,void,constructor
+
+
+
+
 
 
 
---
+++ HRLib/Orders.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+public  class Orders
+    {
+        //Auto implemented - automatically implemented
+        public int OrderNo { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double OrderAmt { get; set; }
+
+        public Orders()
+        {
+            Console.WriteLine("Enter Productid");
+            int prodID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Productname");
+            string prodname = Console.ReadLine();
+            Console.WriteLine("Enter Product Qty");
+            int Qty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Product Price");
+            double price = Convert.ToDouble(Console.ReadLine());
+            double amt = Qty * price;
+            OrderAmt = amt;
+
+
+
+        }
+        int prodid;
+        public Orders(int prodid)
+        {
+            this.prodid = prodid;
+        }
+
+        string pname;
+        public Orders(int prodid,string pname):this(prodid)
+        {
+            this.pname = pname;
+            //this.prodid = prodid;
+            Console.WriteLine(this.prodid);
+            Console.WriteLine(this.pname);
+        }
+
+        float price;
+        public Orders(int prodid,string pname,float price):this(prodid,pname)
+        {
+            this.price = price;
+            Console.WriteLine(this.price);
+        }
+
+
+
+
+        //int,string,double,int
+        //change sequence,change type
+        // int,int,string,double
+        //   double,int,int,string
+
+        public Orders(int prodid,int qty,string name,double price)
+        {
+
+        }
+
+        public Orders(int prodid,string name,double price,int qty)
+        {
+            double amt=price* qty;
+            OrderAmt += amt;
+
+        }
+
+
+
+    }
+}
